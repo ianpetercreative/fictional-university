835b21eafa3b6cc6781d5d76cbc03a0ea9c2cf91
ianmacdonald <ianpetercreative@gmail.com>
2024-05-01 16:02:48 -0600

    add program as a new post type. Adjust the custom query to order them asc by title

--- app/public/wp-content/mu-plugins/university-post-types.php
+++ app/public/wp-content/mu-plugins/university-post-types.php
@@ -1,6 +1,8 @@
 <?php
 
 function university_post_types() {
+
+    // Event Post Type
     register_post_type('event', array(
         'supports' => array('title', 'editor', 'excerpt'),
         'rewrite' => array('slug' => 'events'),
@@ -15,7 +17,27 @@ function university_post_types() {
             'all_items' => 'All Events',
             'singular_name' => 'Event',
             )        
-            ));
-        }
+        )
+    );
+
+
+    // Event Post Type
+    register_post_type('program', array(
+        'supports' => array('title', 'editor'),
+        'rewrite' => array('slug' => 'programs'),
+        'public' => true,
+        'show_in_rest' => true,
+        'menu_icon' => 'dashicons-awards',
+        'has_archive' => true,
+        'labels' => array(
+            'name' => 'Programs',
+            'add_new_item' => 'Add New Program',
+            'edit_item' => 'Edit Program',
+            'all_items' => 'All Programs',
+            'singular_name' => 'Program',
+            )        
+        )
+    );
+}
         
         add_action('init', 'university_post_types');
